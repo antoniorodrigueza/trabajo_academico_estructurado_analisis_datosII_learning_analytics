0ee9ef21f7fa0dccdc6f4ddfb71dc6c6602c6f4c
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 22:07:58 +0200

    Actualizando contenidos

--- tests/test_probador_de_seguridad.py
+++ tests/test_probador_de_seguridad.py
@@ -1,7 +1,11 @@
-from cifrador import codificar_texto, descodificar_texto
+import sys
+sys.path.append('/content/trabajo_academico_estructurado_analisis_datosII_learning_analytics/src')
 
-def comprobar_cifrado(cadena_original):
+from cifrador import codificar_texto as cifrar_texto, descodificar_texto as descifrar_texto
+
+def comprobar_cifrado():
     
+    cadena_original ='Ana'
     valor_cifrado = codificar_texto(cadena_original)
     valor_descifrado = descodificar_texto(valor_cifrado)
 
