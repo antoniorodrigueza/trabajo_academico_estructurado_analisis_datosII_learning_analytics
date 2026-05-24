963d7d3edd21bd59ce3d81205581cbdf1dee8dcc
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 15:42:37 +0200

    Actualizar contenido

--- src/generador_analitica.py
+++ src/generador_analitica.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def filtrar_bajo_rendimiento(tabla_notas):
+    umbral = 5
+    seleccion = tabla_notas[tabla_notas['grade'] < umbral]
+    return seleccion
+
+
+def visualizar_notas(registro):
+    plt.figure(figsize=(8, 5))
+    nombres = registro['name']
+    valores = registro['grade']
+
+    plt.bar(nombres, valores, color='skyblue')
+    plt.title('Rendimiento Académico')
+    plt.xlabel('Alumnado')
+    plt.ylabel('Calificación')
+    plt.tight_layout()
+    plt.show()
 
