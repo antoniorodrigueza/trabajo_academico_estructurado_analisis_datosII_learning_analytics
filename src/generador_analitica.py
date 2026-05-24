c4eadd48b65b97bd543be84ecfb6801c9eb36c3e
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 19:59:09 +0200

    Actualizando contenidos

--- src/generador_analitica.py
+++ src/generador_analitica.py
@@ -1,17 +1,22 @@
 import matplotlib
-matplotlib.use('Agg')  # backend compatible
+import matplotlib
+matplotlib.use('Agg')  # backend compatible con Colab
 import matplotlib.pyplot as plt
 from IPython.display import display
 
+umbral = 5
 
 def filtrar_bajo_rendimiento(tabla_notas):
-    umbral = 5
     seleccion = tabla_notas[tabla_notas['nota'] < umbral]
     return seleccion
 
 def visualizar_notas(registro):
     plt.figure(figsize=(8, 5))
     plt.bar(registro['nombre'], registro['nota'], color='skyblue')
+
+    # Línea roja en el valor del umbral
+    plt.axhline(y=umbral, color='red', linestyle='--', linewidth=2)
+
     plt.title('Rendimiento Académico')
     plt.xlabel('Alumnado')
     plt.ylabel('Calificación')
