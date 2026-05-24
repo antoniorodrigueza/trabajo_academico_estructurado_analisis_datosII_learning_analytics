990435f45b7a00f5df070bc9b42b03d62cbbedfd
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 14:25:52 +0200

    Actualizamos contenido de blockchain.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,1 +1,49 @@
+import hashlib
+import json
+import time
 
+
+class Block:
+    def __init__(self, data, previous_hash=''):
+        self.timestamp = time.time()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_string = json.dumps({
+            'timestamp': self.timestamp,
+            'data': self.data,
+            'previous_hash': self.previous_hash
+        }, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block('Genesis Block', '0')
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        previous_hash = self.get_latest_block().hash
+        new_block = Block(data, previous_hash)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.calculate_hash():
+                return False
+
+            if current.previous_hash != previous.hash:
+                return False
+
+        return True
