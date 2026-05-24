1a90c9a5e0b37b8e9ff9d573547791393c2ea36d
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 22:01:59 +0200

    Actualizando contenidos

--- tests/test_probador_de_seguridad.py
+++ tests/test_probador_de_seguridad.py
@@ -1,10 +1,13 @@
 from cifrador import codificar_texto, descodificar_texto
 
-def comprobar_cifrado():
-    cadena_original = 'Ana'
-
+def comprobar_cifrado(cadena_original):
+    
     valor_cifrado = codificar_texto(cadena_original)
     valor_descifrado = descodificar_texto(valor_cifrado)
 
-    assert cadena_original == valor_descifrado
+    assert cadena_original == valor_descifrado, \
+        f"Error: '{cadena_original}' != '{valor_descifrado}'"
+
+    print("Cifrado y descifrado correctos")
+
 
