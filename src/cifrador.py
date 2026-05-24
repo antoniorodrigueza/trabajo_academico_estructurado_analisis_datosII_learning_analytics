72a6f2da7f5b6f2d9675804365c4b13fe1c9b419
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 15:39:21 +0200

    Actualizamos cifrador.py

--- src/cifrador.py
+++ src/cifrador.py
@@ -1,1 +1,15 @@
+from cryptography.fernet import Fernet
+
+# Crear clave y objeto de cifrado
+clave_segura = Fernet.generate_key()
+motor_cifrado = Fernet(clave_segura)
+
+def codificar_texto(texto_plano):
+    texto_bytes = texto_plano.encode()
+    texto_cifrado = motor_cifrado.encrypt(texto_bytes)
+    return texto_cifrado
+
+def descodificar_texto(texto_cifrado):
+    texto_descifrado = motor_cifrado.decrypt(texto_cifrado)
+    return texto_descifrado.decode()
 
