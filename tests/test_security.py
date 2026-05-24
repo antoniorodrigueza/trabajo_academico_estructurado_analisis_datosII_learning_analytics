4004113847df9fa0dea314eff39bc05928057826
antoniorodrigueza <antonior20a@yahoo.com>
2026-05-24 16:06:01 +0200

    Actualizar contenido

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,1 +1,10 @@
+from cifrador import codificar_texto, descodificar_texto
+
+def comprobar_cifrado():
+    cadena_original = 'Ana'
+
+    valor_cifrado = codificar_texto(cadena_original)
+    valor_descifrado = descodificar_texto(valor_cifrado)
+
+    assert cadena_original == valor_descifrado
 
